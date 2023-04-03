41a839324462ad7a2bc9cef533372fd6b6b29725
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-04-02 23:03:50 -0400

    changed some text on applications.jsx

--- portfolio.react/src/components/applications.jsx
+++ portfolio.react/src/components/applications.jsx
@@ -12,15 +12,10 @@ const Applications = () => {
             <p className="text-4xl font-bold inline border-b-4 border-[#ad8f18]">
               Applications
             </p>
-            <h1 className="text-2xl py-2 ">
-              Here are a few of the applications I've created
-            </h1>
           </div>
           <div></div>
           <div className="max-w-[1000px] w-full px-4 grid gird-cols-2 gap-8"></div>
-          <div className="sm-text-right pb-8 pl-4">
-            
-          </div>
+          <div className="sm-text-right pb-8 pl-4"></div>
         </div>
       </div>
     </div>
